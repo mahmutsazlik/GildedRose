534a5fba85adfd9e3fb2c7b951f1beea95be6e0d
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-05-22 16:48:49 +0300

    Diğer pin-down testler eklendi.
        - Bir öğenin quality değeri hiçbir zaman 50'yi geçmez
        - Efsanevi bir ürün olan "Sulfuras" asla satılmamalı veya quality değerinde düşüş olmamalıdır.
        - "Backstage passes", tıpkı "Aged Brie" gibi, sellIn değeri azaldıkça quality değeri artar;
        quality değeri, 10 gün veya daha az süre kaldığında 2, 5 gün veya daha az süre kaldığında 3 artar
        fakat anlaşma sonrası quality 0'a düşüyor

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -44,4 +44,44 @@ class GildedRoseTest {
         Item item = createAndUpdate("Aged Brie", 15, 25);
         assertEquals(26, item.quality);
     }
+
+    @Test
+    public void qualityNeverMoreThan50() {
+        Item item = createAndUpdate("foo", 15, 52);
+        assertEquals(51, item.quality); // can exceed 50 if input exceeds 50
+
+        item = createAndUpdate("Aged Brie", 15, 50);
+        assertEquals(50, item.quality);
+    }
+
+    @Test
+    public void sulfurasNeverHasToBeSoldOrDecreasesInQuality() {
+        Item item = createAndUpdate("Sulfuras, Hand of Ragnaros", 1, 42);
+        assertEquals(1, item.sellIn); // sellIn doesn't change
+        assertEquals(42, item.quality); // quality doesn't change
+    }
+
+    @Test
+    public void backstagePassesIncreaseInQuality() {
+        Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 15, 25);
+        assertEquals(26, item.quality);
+    }
+
+    @Test
+    public void backstagePassesIncreaseBy2() {
+        Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 25);
+        assertEquals(27, item.quality);
+    }
+
+    @Test
+    public void backstagePassesIncreaseBy3() {
+        Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 25);
+        assertEquals(28, item.quality);
+    }
+
+    @Test
+    public void backstagePassesQualityDropTo0() {
+        Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 0, 25);
+        assertEquals(0, item.quality);
+    }
 }
