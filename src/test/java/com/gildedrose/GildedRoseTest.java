8cd378e8f20b30b43832fe5f8582bae3af225625
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-05-22 14:22:56 +0300

    DRY(Don't repeat yourselves) prensibine uygun code refactoring yapıldı.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,36 +6,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void testFrameworkWorks() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+
+    private Item createAndUpdate(int sellIn, int quality) {
+        Item[] items = new Item[] { new Item("foo", sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return items[0];
+    }
+
+    @Test
+    void testFrameworkWorks() {
+        Item item = createAndUpdate(0,0);
+        assertEquals("foo", item.name);
     }
 
     @Test
     public void systemLowersValues(){
-        Item[] items = new Item[]{new Item("foo",15,25)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(14,app.items[0].sellIn);
-        assertEquals(24,app.items[0].quality);
+        Item item = createAndUpdate(15,25);
+        assertEquals(14,item.sellIn);
+        assertEquals(24,item.quality);
     }
 
     @Test
     public void qualityDegradesTwiceAsFast(){
-        Item[] items = new Item[]{new Item("foo",0,17)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(15,app.items[0].quality);
+        Item item = createAndUpdate(0,17);
+        assertEquals(15,item.quality);
     }
 
     @Test
     public void qualityIsNeverNegative(){
-        Item[] items = new Item[]{new Item("foo",0,0)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(0,app.items[0].quality);
+        Item item = createAndUpdate(0,0);
+        assertEquals(0,item.quality);
     }
 }
