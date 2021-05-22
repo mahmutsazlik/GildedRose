0299d0d21e2fa7e9d2572c7c682665d30995e467
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-05-22 15:56:03 +0300

    agedBrieIncreasesInQuality testi eklendi.
    Code refactoring
    -Change Signature: Change a method by adding or removing input parameters.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
 
-    private Item createAndUpdate(int sellIn, int quality) {
-        Item[] items = new Item[] { new Item("foo", sellIn, quality) };
+    private Item createAndUpdate(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         return items[0];
@@ -16,26 +16,32 @@ class GildedRoseTest {
 
     @Test
     void testFrameworkWorks() {
-        Item item = createAndUpdate(0,0);
+        Item item = createAndUpdate("foo", 0, 0);
         assertEquals("foo", item.name);
     }
 
     @Test
-    public void systemLowersValues(){
-        Item item = createAndUpdate(15,25);
-        assertEquals(14,item.sellIn);
-        assertEquals(24,item.quality);
+    public void systemLowersValues() {
+        Item item = createAndUpdate("foo", 15, 25);
+        assertEquals(14, item.sellIn);
+        assertEquals(24, item.quality);
     }
 
     @Test
-    public void qualityDegradesTwiceAsFast(){
-        Item item = createAndUpdate(0,17);
-        assertEquals(15,item.quality);
+    public void qualityDegradesTwiceAsFast() {
+        Item item = createAndUpdate("foo", 0, 17);
+        assertEquals(15, item.quality);
     }
 
     @Test
-    public void qualityIsNeverNegative(){
-        Item item = createAndUpdate(0,0);
-        assertEquals(0,item.quality);
+    public void qualityIsNeverNegative() {
+        Item item = createAndUpdate("foo", 0, 0);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    public void agedBrieIncreasesInQuality() {
+        Item item = createAndUpdate("Aged Brie", 15, 25);
+        assertEquals(26, item.quality);
     }
 }
